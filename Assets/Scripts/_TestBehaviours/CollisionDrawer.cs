01eb6ee9e1076257f94c2d2b0fc6ae8212993f75
tenko007 <tenko2007@gmail.com>
2022-02-02 21:48:13 +0300

    Added Epic Toon FX - explosions

---
+++ Assets/Scripts/_TestBehaviours/CollisionDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDrawer : MonoBehaviour
+{
+    void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+            // Visualize the contact point
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+    }
+}
